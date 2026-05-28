33d81965c3a3cbc4a03a2d9c21ed84124fd57cda
unknown <jitikawa01@gmail.com>
2026-05-28 11:15:33 -0300

    Lista dinâmica com botão de concluir e remover

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-
+import { useState } from "react";
 
 export function App() {
+  const [value, setValue] = useState('')
+ const [list, setList] = useState([
+  {id: 1, label: 'Fazer café', completed: false},
+  {id: 2, label: 'Fazer café', completed: false},
+  {id: 3, label: 'Fazer almoço', completed: false},
+  {id: 4, label: 'Fazer jantar', completed: false}
+ ]);
+
+
 
   return (
-    <>
-    Olá
-    </>
+    <div>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => {
+        setList([...list,{id: (list.length + 1), label: value, completed: false}])
+        setValue('')
+      }}
+      >Adicionar</button>
+
+      <ol>
+        {list.map((listItem: {id: number, label: string, completed: boolean}) => (
+          <li key={listItem.id}>{listItem.label} 
+
+          {listItem.completed ? 'Concluido' : ''} 
+
+            <button onClick={() => setList([...list.map(item => ({ ...item, completed: item.id === listItem.id ? true : item.completed}))])}>Concluir</button> 
+            <button onClick={() => setList([...list.filter(item => item.id !== listItem.id)])}>Remover</button> 
+          </li>
+        ))}
+      </ol>
+    </div>
   )
 }
+
+
 
